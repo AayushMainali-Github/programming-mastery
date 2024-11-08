47b08404257a06901bb04338df09811c387c4c14
New 6969 <aayushmainali.codes@gmail.com>
2024-11-08 23:29:05 +0545

    Routing

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../Screens/Home/Home";
+import Javascript from "../Screens/Javascript/Javascript";
+import Python from "../Screens/Python/Python";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/javascript" element={<Javascript />} />
+        <Route path="/python" element={<Python />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
