bb2ec6b2c8cb22a0d4adc1cbc52a7b48355205ce
New 6969 <aayushmainali.codes@gmail.com>
2024-11-09 00:16:47 +0545

    4 JS Chapters added

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,15 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../Screens/Home/Home";
 import Javascript from "../Screens/Javascript/Javascript";
 import Python from "../Screens/Python/Python";
+import Introduction from "../Screens/Javascript/Chapters/Introduction";
+import Variables from "../Screens/Javascript/Chapters/Variables";
+import Datatypes from "../Screens/Javascript/Chapters/Datatypes";
+import Comments from "../Screens/Javascript/Chapters/Comments";
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/javascript" element={<Javascript />} />
-        <Route path="/python" element={<Python />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/javascript" element={<Javascript />} />
+          <Route path="/javascript/introduction" element={<Introduction />} />
+          <Route path="/javascript/variables" element={<Variables />} />
+          <Route path="/javascript/datatypes" element={<Datatypes />} />
+          <Route path="/javascript/comments" element={<Comments />} />
+          <Route path="/python" element={<Python />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
